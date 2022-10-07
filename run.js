b084ebf3d0d305ab26dbb40c36bcebccf869f3d1
Leo Mark Castro <66856556+leomarkcastro@users.noreply.github.com>
2022-10-07 11:17:50 +0800

    pm2 ready deployment

--- run.js
+++ run.js
@@ -1,6 +1,4 @@
-const {series} = require('async');
-const {exec} = require('child_process');
+const { series } = require("async");
+const { exec } = require("child_process");
 
-series([
- () => exec(`PORT=${process.env.PORT || "4343"} npm start`),
-]); 
+series([() => exec(`PORT=${process.env.PORT || "4343"} npm start`)]);
